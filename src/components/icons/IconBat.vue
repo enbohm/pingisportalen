e7287ab74657d72b7624b9e226f86cb6c7fc349b
enbohm <andreas@enbohms.se>
2022-11-11 14:52:13 +0100

    Formatting code

--- src/components/icons/IconBat.vue
+++ src/components/icons/IconBat.vue
@@ -1,8 +1,3 @@
 <template>
-  <img
-  src="./bat.svg"
-  alt="Table tennis bat"
-  height="87"
-  width="100"
-  padding="100" />
+  <img src="./bat.svg" alt="Table tennis bat" height="87" width="100" padding="100" />
 </template>
