05027aae20c6226fd5d304ee48f1fdef544c99ec
enbohm <andreas@enbohms.se>
2022-11-11 13:11:49 +0100

    Name refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import PPWelcome from './components/PPWelcome.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Hello from pingisportalen!!" />
+      <PPWelcome msg="Välkommen till Pingisportalen!" />
     </div>
   </header>
 
