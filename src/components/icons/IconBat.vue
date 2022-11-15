e80fd2ae6f207c89eda5a609df47e38526dbb3df
enbohm <andreas@enbohms.se>
2022-11-15 10:22:33 +0100

    Minor styling fixes and removed PPWelcom

--- src/components/icons/IconBat.vue
+++ src/components/icons/IconBat.vue
@@ -1,3 +1,12 @@
 <template>
-  <img src="./bat.svg" alt="Table tennis bat" height="87" width="100" padding="100" />
+  <div id="baticon">
+    <img src="./bat.svg" alt="Table tennis bat" height="87" width="100" padding="100" />
+  </div>
 </template>
+
+<style scoped>
+#baticon {
+  text-align: center;
+  margin-top: 10%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,40 @@
 <script setup>
-import PPWelcome from '../components/PPWelcome.vue'
 import IconBat from '../components/icons/IconBat.vue'
 </script>
 
 <template>
-  <PPWelcome msg="Välkommen till Pingisportalen!" />
+
+  <div class="greetings">
+    <h1 class="green">Välkommen till Pingisportalen!</h1>
+    <h3>
+      Pingisportalen - stället för alla pingisintresserade! Här finns en massa bra länkar och information om bordtennis
+      - den enda sida du behöver om du gillar pingis :)
+    </h3>
+  </div>
   <IconBat />
 </template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+}
+</style>
