4d6098936881c60535ac49956666b0d1290b61d1
enbohm <andreas@enbohms.se>
2022-11-17 08:59:00 +0100

    Adding hardcoded chart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey"
+    :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 600
+    },
+    height: {
+      type: Number,
+      default: 600
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => { }
+    },
+    plugins: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['Halmstad', 'Rekordspelen', 'Ängby', 'Flyman', 'Söderspelen', 'Eslövsspelen', 'Safir', 'Spårvägsspelen'],
+        datasets: [{
+          data: [9, 16, 9, 0, 0, 0, 0, 0], 
+          backgroundColor: '#33BBFF', 
+          label: "Jane Doe"
+        }, 
+        {
+          data: [9, 5, 55, 0, 0, 0, 0, 0], 
+          backgroundColor: 'red', 
+          label: "John Doe"
+        }],
+        chartId: "bar-chart"
+      },
+      chartOptions: {
+        responsive: false,
+      }
+    }
+  }
+}
+</script>
--- src/views/SwedenTour.vue
+++ src/views/SwedenTour.vue
@@ -1,3 +1,17 @@
+<script>
+import BarChart from '../components/BarChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BarChart
+  }
+}
+</script>
 <template>
-    <h2>Sweden Tour</h2>
-  </template>
+  <h2>Sweden Tour</h2>
+  <p>Information om Sweden Tour kan du hitta på <a
+      href="https://www.svenskbordtennis.com/tavling/nationellatavlingar/SwedenTour/" target="_blank">denna
+      länk</a>. Nedan finns statisk om de olika spelarna på touren.</p>
+  <BarChart height="600" width="600"/>
+</template>
