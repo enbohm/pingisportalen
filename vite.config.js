0e34258417d354a4fbde8446cbcf6343aff2f8d9
enbohm <andreas@enbohms.se>
2022-11-25 11:19:01 +0100

    Adding comment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dsv from '@rollup/plugin-dsv'
 
-// https://vitejs.dev/config/
+// for options see https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   build: {
