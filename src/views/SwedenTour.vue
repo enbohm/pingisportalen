6e0ad16ae20fb9dc40bf73eacc7e60ca247c0e97
enbohm <andreas@enbohms.se>
2023-04-03 11:43:54 +0200

    Fixing another spelling issue

--- src/views/SwedenTour.vue
+++ src/views/SwedenTour.vue
@@ -16,7 +16,7 @@ export default {
     <p>Sweden Tour är samlingsnamnet för svenska elittävlingar i bordtennis och omfattar både senior- och
       ungdomsklasser. Sweden Tour spelas genom ett antal
       deltävlingar där spelarena samlar poäng i varje tävling (totalt 8 deltävlingar). Tolv spelare som samlat mest poäng 
-      (räknas ihop genom att ta de sex tävlingar där spelaren samlat mest poäng bäst) möts sedan i 
+      (räknas ihop genom att ta de sex tävlingar där spelaren samlat mest poäng) möts sedan i 
       Tourfinalerna. Mer information om Sweden Tour kan du
       hitta på <a href="https://www.svenskbordtennis.com/tavling/nationellatavlingar/SwedenTour/" target="_blank">denna
         länk</a>.
